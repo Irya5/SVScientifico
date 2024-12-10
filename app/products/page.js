413ae6a7328ec1ddb26ac5602c455e8b250cc941
Irya5 <jainirya5@gmail.com>
2024-12-10 13:24:16 +0530

    update product page

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,6 @@
 'use client';  // Ensures that the code runs on the client side
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation'; // Using useSearchParams hook
 import { MdOutlineSearch } from 'react-icons/md';
 import ContactSection from '@/components/ContactSection';
@@ -94,72 +94,74 @@ export default function Products() {
 	};
 
 	return (
-		<main className="bg-[#FAFCFF] md:px-24 py-20 md:px-24 px-4">
-			<div className="text-[#242E49]">
-				<ul className="text-sm list-disc pl-4 hidden md:block font-gt-super">
-					<li>Products</li>
-				</ul>
-				<div className="text-[36px] md:text-[48px] font-[500] text-center md:text-left leading-[50px] mt-4 w-full md:w-1/2">
-					Unmatched Quality in <br />
-					Every Product
+		<Suspense fallback={<div>Loading...</div>}>
+			<main className="bg-[#FAFCFF] md:px-24 py-20 md:px-24 px-4">
+				<div className="text-[#242E49]">
+					<ul className="text-sm list-disc pl-4 hidden md:block font-gt-super">
+						<li>Products</li>
+					</ul>
+					<div className="text-[36px] md:text-[48px] font-[500] text-center md:text-left leading-[50px] mt-4 w-full md:w-1/2">
+						Unmatched Quality in <br />
+						Every Product
+					</div>
 				</div>
-			</div>
 
-			<div className="grid md:grid-cols-4 grid-cols-1 gap-20 mt-16 md:mt-28 md:px-20 px-8">
-				{/* Categories section */}
-				<div className="col-span-1 flex flex-col gap-y-3">
-					{categories.map((categoryName, index) => (
-						<div
-							key={index}
-							className={`cursor-pointer rounded-[12px] text-center font-[700] text-lg py-3.5 transition-all duration-300 ${activeCategory === categoryName
-									? 'bg-black text-white' // Active category styles
-									: 'bg-[#F2F5F9] text-[#242E49] hover:bg-[#d9dce0] hover:text-black'
-								}`}
-							onClick={() => handleCategoryClick(categoryName)} // Update category and URL
-						>
-							{categoryName}
+				<div className="grid md:grid-cols-4 grid-cols-1 gap-20 mt-16 md:mt-28 md:px-20 px-8">
+					{/* Categories section */}
+					<div className="col-span-1 flex flex-col gap-y-3">
+						{categories.map((categoryName, index) => (
+							<div
+								key={index}
+								className={`cursor-pointer rounded-[12px] text-center font-[700] text-lg py-3.5 transition-all duration-300 ${activeCategory === categoryName
+										? 'bg-black text-white' // Active category styles
+										: 'bg-[#F2F5F9] text-[#242E49] hover:bg-[#d9dce0] hover:text-black'
+									}`}
+								onClick={() => handleCategoryClick(categoryName)} // Update category and URL
+							>
+								{categoryName}
+							</div>
+						))}
+					</div>
+
+					{/* Products section */}
+					<div className="col-span-1 md:col-span-3 flex flex-col">
+						<div className="flex gap-2 border-2 border-[#E4E8ED] rounded-[8px] p-3 text-[#242E49]">
+							<MdOutlineSearch size={24} color="#C5CAD3" />
+							<input
+								className="outline-none text-[#242E49] bg-transparent"
+								type="text"
+								placeholder="Search . . ."
+								value={searchQuery}
+								onChange={handleSearchChange}
+							/>
 						</div>
-					))}
-				</div>
 
-				{/* Products section */}
-				<div className="col-span-1 md:col-span-3 flex flex-col">
-					<div className="flex gap-2 border-2 border-[#E4E8ED] rounded-[8px] p-3 text-[#242E49]">
-						<MdOutlineSearch size={24} color="#C5CAD3" />
-						<input
-							className="outline-none text-[#242E49] bg-transparent"
-							type="text"
-							placeholder="Search . . ."
-							value={searchQuery}
-							onChange={handleSearchChange}
-						/>
-					</div>
-
-					<div className="flex flex-col mt-8">
-						{loading ? (
-							<div>Loading products...</div>
-						) : filteredProducts.length === 0 ? (
-							<div>No products found</div>
-						) : (
-							filteredProducts.map((product, index) => (
-								<div key={index} className="flex items-center justify-between border-b py-6 border-[#DCE1E8]">
-									<div className="flex flex-col gap-2">
-										<div className="text-[#242E49] font-[700] text-lg">{product.ProductName}</div>
-										<div className="text-[#5D6A85] text-lg">Pack: {product.Pack}</div>
+						<div className="flex flex-col mt-8">
+							{loading ? (
+								<div>Loading products...</div>
+							) : filteredProducts.length === 0 ? (
+								<div>No products found</div>
+							) : (
+								filteredProducts.map((product, index) => (
+									<div key={index} className="flex items-center justify-between border-b py-6 border-[#DCE1E8]">
+										<div className="flex flex-col gap-2">
+											<div className="text-[#242E49] font-[700] text-lg">{product.ProductName}</div>
+											<div className="text-[#5D6A85] text-lg">Pack: {product.Pack}</div>
+										</div>
+										<div className="border cursor-pointer border-[#5D6A85] rounded-[8px] px-5 py-2 bg-white font-[500] text-[#242E49]">
+											Get Quote
+										</div>
 									</div>
-									<div className="border cursor-pointer border-[#5D6A85] rounded-[8px] px-5 py-2 bg-white font-[500] text-[#242E49]">
-										Get Quote
-									</div>
-								</div>
-							))
-						)}
+								))
+							)}
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div>
-				<ContactSection />
-			</div>
-		</main>
+				<div>
+					<ContactSection />
+				</div>
+			</main>
+		</Suspense>
 	);
 }
